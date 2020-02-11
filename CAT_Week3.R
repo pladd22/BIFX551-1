df6dae4e1328ef6bcdf5737b5adea70d1a0b71c5
caltimmons <40839829+caltimmons@users.noreply.github.com>
2020-02-11 16:07:43 -0500

    Add files via upload

--- CAT_Week3.R
+++ CAT_Week3.R
@@ -1,3 +1,4 @@
+#Dataset from https://github.com/rmcelreath/rethinking/tree/master/data
 fgs <- read.delim('fieldgoals.csv', sep = ';')
 summary(fgs)
 #The average player seems to make a little less than half the shots that they take.  Even the best seems to miss about a third.
