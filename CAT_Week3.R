8fd622d61985376a5cde427601d0ce230f7ba8cb
caltimmons <40839829+caltimmons@users.noreply.github.com>
2020-02-11 16:06:41 -0500

    Add files via upload

---
+++ CAT_Week3.R
@@ -0,0 +1,8 @@
+fgs <- read.delim('fieldgoals.csv', sep = ';')
+summary(fgs)
+#The average player seems to make a little less than half the shots that they take.  Even the best seems to miss about a third.
+fgs$FGPer <- fgs$FGM/fgs$FGA
+plot(fgs$FGM ~ fgs$FGA,
+     xlab = "Fieldgoal Attempts",
+     ylab = 'Fieldgoals Made',
+     main = 'Fieldgoals Made vs Attempts')
