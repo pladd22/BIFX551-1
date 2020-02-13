b15582eddf7f0ebc36cb07495ea7ac5227726443
Chris Malec <43046089+cemalec@users.noreply.github.com>
2020-02-12 20:48:16 -0500

    Update CAT_Week3.R

--- week3/CAT_Week3.R
+++ week3/CAT_Week3.R
@@ -1,5 +1,5 @@
 #Dataset from https://github.com/rmcelreath/rethinking/tree/master/data
-fgs <- read.delim('fieldgoals.csv', sep = ';')
+fgs <- read.delim('~/files/fieldgoals.csv', sep = ';')
 summary(fgs)
 #The average player seems to make a little less than half the shots that they take.  Even the best seems to miss about a third.
 fgs$FGPer <- fgs$FGM/fgs$FGA
